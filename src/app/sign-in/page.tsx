9938ab6e07f3c262ed86654a128dbf77870966cb
Caleb Rivera <calebrossr@gmail.com>
2024-06-08 02:06:27 -0700

    Add shadcn components

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -1,3 +1,12 @@
+import { Input } from "~/components/ui/input";
+
 export default async function SignInPage() {
-  return <main>Sign in</main>;
+  return (
+    <main className="grid min-h-screen place-content-center">
+      <div>
+        <Label htmlFor="email">Email</Label>
+        <Input type="email" id="email" placeholder="Email" />
+      </div>
+    </main>
+  );
 }
