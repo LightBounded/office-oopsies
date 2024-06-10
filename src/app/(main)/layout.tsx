b1bca2d3065e1de341819a38ea6f54698a4768a8
Caleb Rivera <calebrossr@gmail.com>
2024-06-09 19:52:29 -0700

    Update navbar

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,4 +1,6 @@
 import Link from "next/link";
+import { Button } from "~/components/ui/button";
+import { signOutAction } from "~/server/auth/sign-out";
 
 export default function MainLayout({
   children,
@@ -15,8 +17,11 @@ export default function MainLayout({
 
 function Navbar() {
   return (
-    <nav className="sticky top-0 mb-4 border-b border-border/40  px-8 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <ul className="flex h-14 items-center mx-auto gap-4">
+    <nav className="sticky top-0 mb-4 flex justify-between border-b border-border/40  px-8 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+      <Link href="/" className="flex items-center text-xl font-bold">
+        Office Oopsies
+      </Link>
+      <ul className="flex h-14 items-center gap-4">
         <li>
           <Link href="/">Home</Link>
         </li>
@@ -25,6 +30,20 @@ function Navbar() {
         </li>
         <li>
           <Link href="/profile">Profile</Link>
+        </li>
+      </ul>
+      <ul className="flex items-center">
+        <li>
+          <form>
+            <Button
+              formAction={async () => {
+                "use server";
+                await signOutAction();
+              }}
+            >
+              Sign Out
+            </Button>
+          </form>
         </li>
       </ul>
     </nav>
