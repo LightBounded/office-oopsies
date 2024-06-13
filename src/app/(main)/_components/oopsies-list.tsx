7fc7d95d0b4b728679ebac54fe57fd764d0e5e8b
Caleb Rivera <calebrossr@gmail.com>
2024-06-12 23:46:24 -0700

    Update oopsie card styles

--- src/app/(main)/_components/oopsies-list.tsx
+++ src/app/(main)/_components/oopsies-list.tsx
@@ -38,34 +38,35 @@ function OopsieCard({
   });
 
   return (
-    <div className="relative min-h-36 rounded-md border p-6">
-      <div className="mb-2 sm:mb-0">
+    <div className="relative flex min-h-36 flex-col rounded-md border p-6 sm:flex-row sm:gap-6">
+      <div className="mb-1">
         <div className="text-xl font-bold">{oopsie.user.username}</div>
         <div>{oopsie.description}</div>
       </div>
-
-      <div className="absolute right-6 top-6 text-sm text-foreground/80">
-        Reported by{" "}
-        <Link
-          className="underline underline-offset-4"
-          href={`/profile/${oopsie.author.username}`}
-        >
-          {oopsie.author.username}
-        </Link>
-      </div>
-      <div className="absolute bottom-6 right-6 space-x-2">
-        <Button
-          onClick={() => likeOopsie.mutate(oopsie.id)}
-          size="sm"
-          className="h-8"
-        >
-          <ThumbsUpIcon className="mr-2 h-4 w-4" />
-          {oopsie.likes}
-        </Button>
-        <Button size="sm" className="h-8">
-          <MessageCircleIcon className="mr-2 h-4 w-4" />
-          Comment
-        </Button>
+      <div className="sm:ml-auto sm:flex sm:flex-col">
+        <div className="mb-4 text-nowrap text-sm text-foreground/80">
+          Reported by{" "}
+          <Link
+            className="underline underline-offset-4"
+            href={`/profile/${oopsie.author.username}`}
+          >
+            {oopsie.author.username}
+          </Link>
+        </div>
+        <div className="mt-auto space-x-2 text-nowrap">
+          <Button
+            onClick={() => likeOopsie.mutate(oopsie.id)}
+            size="sm"
+            className="h-8"
+          >
+            <ThumbsUpIcon className="mr-2 h-4 w-4" />
+            {oopsie.likes}
+          </Button>
+          <Button size="sm" className="h-8">
+            <MessageCircleIcon className="mr-2 h-4 w-4" />
+            Comment
+          </Button>
+        </div>
       </div>
     </div>
   );
